5e39caa1318ef075f554a9a816d942e43c23b969
Austin Kregel <austinkregel@users.noreply.github.com>
2017-12-27 00:05:57 +0800

    Apply fixes from StyleCI (#1)

--- tests/Feature/FormSubmissionTest.php
+++ tests/Feature/FormSubmissionTest.php
@@ -2,38 +2,36 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use App\Mail\Message;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
 use Tests\Traits\InteractsWithContactAndAssertStatus;
 
 class FormSubmissionTest extends TestCase
 {
     use RefreshDatabase, InteractsWithContactAndAssertStatus;
-    
+
     /**
      * @var array
      */
     protected $contactData = [
         'message' => 'This is a message',
-        'name' => 'John Smith',
-        'email' => 'john.smith@example.com',
-        'number' => '989-493-4690',
+        'name'    => 'John Smith',
+        'email'   => 'john.smith@example.com',
+        'number'  => '989-493-4690',
     ];
-    
-    
+
     /**
-     * Make sure we can post to the contact form
+     * Make sure we can post to the contact form.
      */
     public function test_if_we_can_submit_the_form()
     {
         $this->touchApiAndAssert($this->contactData);
     }
-    
+
     /**
-     * Make sure we can post to the contact form
+     * Make sure we can post to the contact form.
      */
     public function test_if_we_can_submit_the_form_and_we_are_missing_the_number()
     {
@@ -41,44 +39,44 @@ class FormSubmissionTest extends TestCase
 
         $this->assertDatabaseMissing('messages', ['number' => '989-493-4690']);
     }
-    
+
     /**
-     * See if we get an 422 for invalid data
+     * See if we get an 422 for invalid data.
      */
     public function test_if_we_get_a_422_for_invalid_data()
     {
         $contact = array_merge(
             array_only($this->contactData, ['name', 'message']), [
-                'eml' => 'john.smith@example.com'
+                'eml' => 'john.smith@example.com',
             ]
         );
-        
+
         $this->touchApiAndAssert($contact, 422);
-        
+
         $this->assertDatabaseMissing('messages', $contact);
     }
-    
+
     /**
-     * See if we insert the phone number into the database
+     * See if we insert the phone number into the database.
      */
     public function test_if_we_can_see_the_number_in_the_DB()
     {
         $this->touchApiAndAssert($this->contactData);
-        
+
         $this->assertDatabaseHas('messages', $this->contactData);
     }
-    
+
     /**
-     * Make sure we send the mail
+     * Make sure we send the mail.
      */
-    public function test_if_we_can_send_an_email() 
+    public function test_if_we_can_send_an_email()
     {
         Mail::fake();
 
         $response = $this->touchApiAndAssert($this->contactData);
 
         $message = $response->json();
-        
+
         Mail::assertSent(Message::class, function ($mail) use ($message) {
             return $mail->message->id === $message['id'];
         });
--- tests/Traits/InteractsWithContactAndAssertStatus.php
+++ tests/Traits/InteractsWithContactAndAssertStatus.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Traits;
 
-trait InteractsWithContactAndAssertStatus 
+trait InteractsWithContactAndAssertStatus
 {
     /**
-     * Make sure we can post to the contact form
-     * 
+     * Make sure we can post to the contact form.
+     *
      * @return $response
      */
     public function touchApiAndAssert($data, $status = 200)
@@ -14,7 +14,7 @@ trait InteractsWithContactAndAssertStatus
         $response = $this->json('POST', '/api/contact', $data);
 
         $response->assertStatus($status);
-        
+
         return $response;
     }
 }
