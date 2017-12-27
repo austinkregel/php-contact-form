6b1c85a325b76b6b5007ae26c2bcd5199255140a
Austin Kregel <austinkregel@users.noreply.github.com>
2017-12-27 09:21:04 +0800

    Apply fixes from StyleCI (#7)
    [ci skip] [skip ci]

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -3,13 +3,13 @@
 namespace Tests\Feature;
 
 use App\User;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class AdminTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -18,12 +18,12 @@ class AdminTest extends TestCase
     public function test_we_can_force_login()
     {
         $response = $this->get('/force-login');
-        
+
         $response->assertStatus(302);
-        
+
         $this->assertNotNull(User::first());
     }
-    
+
     /**
      * A basic test example.
      *
@@ -32,7 +32,7 @@ class AdminTest extends TestCase
     public function test_we_can_see_the_login_page()
     {
         $response = $this->get('/login');
-        
-        $response->assertSee('Admin Login') ;
+
+        $response->assertSee('Admin Login');
     }
 }
